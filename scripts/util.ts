0efe4efc1f2a1874595b3414d7641e8eadde1bbe
stagas <gstagas@gmail.com>
2021-12-07 07:23:58 +0200

    fix: name is not a boolean

--- scripts/util.ts
+++ scripts/util.ts
@@ -183,7 +183,9 @@ const special = {
   role: typeref('HTMLRole'),
   sandbox: typeref('HTMLIframeSandbox'),
   style: factory.createUnionTypeNode([typeref('CSSProperties'), factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword)]),
-  type: factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
+  type: factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword), // TODO: this should be fixed in markuplint
+  name: factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword), // TODO: this should be fixed in markuplint
+  // TODO: this should be fixed in markuplint
   value: factory.createUnionTypeNode([
     factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
     factory.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword),
